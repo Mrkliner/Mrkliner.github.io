302ab0a28ca6228deacdbfc94895cff3d8f1b146
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-03-17 15:47:57 +0300

    Create print.php

---
+++ print.php
@@ -0,0 +1,9 @@
+<?php
+  $page_id = 150;// Уникальный идентификатор страницы (статьи или поста)
+  $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
+  $result_set = $mysqli->query("SELECT * FROM `comments` WHERE `page_id`='$page_id'"); //Вытаскиваем все комментарии для данной страницы
+  while ($row = $result_set->fetch_assoc()) {
+    print_r($row); //Вывод комментариев
+    echo "<br />";
+  }
+?>
