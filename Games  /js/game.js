25fd49021448eca5f1118d3b6da91c98ba160f7d
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-04-03 23:35:24 +0300

    Create game.js

---
+++ Games  /js/game.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("game");
+const stx = canvas.getContext("2d");
+const ground = new Image();
+ground.scr = "Mrkliner.github.io/Game/img/ground.png";
+const food = new Image();
+food.scr = "Mrkliner.github.io/Game/img/food.png";
+let box = 32;
+let score = 0;
+function DrawGame() {
+  ctx.drawImage(ground, 0, 0);
+}
