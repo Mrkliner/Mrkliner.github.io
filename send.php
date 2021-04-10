15968ad69eb482c5e96e3d1d6ec2fdae9a855c8e
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-04-10 23:12:54 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,16 +1,4 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="text" name="fio" placeholder="Укажите ФИО" required>
-<input type="text" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
 } else {
  //показываем форму
  $fio = $_POST['fio'];
@@ -21,12 +9,10 @@ if(!isset($_POST['fio']) and !isset($_POST['email'])){
  $email = urldecode($email);
  $fio = trim($fio);
  $email = trim($email);
- if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
+ if (mail("agarrismodov@gmail.ru", "Заявка с сайта", "oтзыв:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
  echo "Сообщение успешно отправлено";
  } else {
  echo "При отправке сообщения возникли ошибки";
  }
 }
 ?>
-</body>
-</html>
