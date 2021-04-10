b74fd8330b07a5861e35b7e6eea6a472ec22c95d
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-04-10 23:08:47 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("agarrismodov@gmail.ru", "Заявка с сайта", "отзыв:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
