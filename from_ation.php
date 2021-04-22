baca44cf4c800a96398121c8ebe7981f5cbd273e
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-04-22 14:04:53 +0300

    Create from_ation.php

---
+++ from_ation.php
@@ -0,0 +1,7 @@
+<?
+else
+{
+   header ('Location: Mrkliner.github.io/Testificate.TXT');  // перенаправление на нужную страницу
+   exit();    // прерываем работу скрипта, чтобы забыл о прошлом
+}
+?>
