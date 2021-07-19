79d3c89802b6e1f6ba9d9bccbd3841a6806c0571
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-07-19 15:44:14 +0300

    Create Meta.js

---
+++ Pages/MrklinerTools/Meta.js
@@ -0,0 +1,22 @@
+ const fileUploader = document.getElementById('file-uploader');
+
+fileUploader.addEventListener('change', (event) => {
+  const files = event.target.files;
+  console.log('files', files);
+  
+  for (const file of files) {
+    const name = file.name;
+    const type = file.type ? file.type : 'NA';
+    const size = file.size;
+    const lastModified = file.lastModified;
+    console.log({file, name, type, size, lastModified});
+    
+    const feedback = document.getElementById('feedback');
+    const msg = ` File Name: ${name} <br/>
+              File Size: ${size} <br/>
+              File type: ${type} <br/>
+              File Last Modified: ${new Date(lastModified)}`;
+              
+    feedback.innerHTML = msg;
+  }
+});
