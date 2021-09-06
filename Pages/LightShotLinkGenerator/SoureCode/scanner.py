50f11cde9543fab9212249c7a21291f8bc9fd723
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2021-09-06 19:00:02 +0300

    Add files via upload

---
+++ Pages/LightShotLinkGenerator/SoureCode/scanner.py
@@ -0,0 +1,142 @@
+import os
+def scanMain():
+	cls()
+	print ("Версия не имеет право использоваться против пользователей сети!")
+	print ("                     By Mrkliner                        ")
+	print ("    / / / /     |    /             _            |      |")
+	print ("   / /          |   /             / \\           |      |")
+	print ("  / /           |  /             /   \\          |      |")
+	print (" / /            | /             /     \\         |      |")
+	print ("/ /             |/             /_______\\        |______|")
+	print ("\\ \\             |\\            /         \\       |      |")
+	print (" \\ \\            | \\          |           |      |      |")
+	print ("  \\ \\           |  \\         |           |      |      |")
+	print ("   \\ \\ \\ \\      |   \\        |           |      |      |")
+	print ("                     PrintScreen.com                    ")
+	print ("Лицензионное соглашение:\nВы не должны использовать программу против законодательства вашей страны.\nВыразите согласие или несогласие с Лицензионным соглашением! 1 - Согласен\n2 - не согласен")
+	variant = int(input(">>"))
+	if variant == 1:
+		cls()
+		scaner();
+	elif variant == 2:
+		exit()
+	else:
+		cls()
+		cls()
+		scanMain()
+def cls():
+	os.system('clear');
+	os.system('cls');
+def scaner():
+	linknum = 0
+	#Links end
+
+	a = 'a'
+	b = 'b'
+	c = 'c' 
+	d = 'd'
+	e = 'e'
+	f = 'f'
+	g = 'g'
+	h = 'h'
+	k = 'k'
+	m = 'm'
+	n = 'n'
+	o = 'o'
+	q = 'q'
+	s = 's'
+	u = 'u'
+	x = 'x'
+	w = 'w'
+	z = 'z'
+	ab = 'ab'
+	ba = 'ba'
+	ac = 'ac'
+	ca = 'ca'
+	ad = 'ad'
+	da = 'da'
+	ae = 'ae'
+	ea = 'ea'
+	af = 'af'
+	fa = 'fa'
+	ag = 'ag'
+	ga = 'ga'
+	ah = 'ah'
+	ha = 'ha'
+	ia = 'ia'
+	aj = 'aj'
+	ja = 'ja'
+	ak = 'ak'
+	ka = 'ka'
+	am = 'am'
+	ma = 'ma'
+	#End of links end
+	print ("Список функций:\n\t1 - начать сканирование ссылок c числом в конце\n\t2 - начать сканирование ссылок с буквой в конце\n\t3 - очистить экран\n\t4 - выключить утилиту")
+	variant = int(input(">>"))
+	if variant == 1:
+		print ("\n_____________________________________________\n\n")
+		while True:
+			linknum += 1;
+			print ("\t\thttps://prnt.sc/lzpfh" + str(linknum))
+			if linknum == 9:
+				break
+		print ("\n_____________________________________________\n\n")
+	if variant == 2:
+		print ("\n_____________________________________________\n\n")
+		print ("\t\thttps://prnt.sc/lzpf" + a)
+		print ("\t\thttps://prnt.sc/lzpf" + b)
+		print ("\t\thttps://prnt.sc/lzpf" + c)
+		print ("\t\thttps://prnt.sc/lzpf" + d)
+		print ("\t\thttps://prnt.sc/lzpf" + e)
+		print ("\t\thttps://prnt.sc/lzpf" + g)
+		print ("\t\thttps://prnt.sc/lzpf" + h)
+		print ("\t\thttps://prnt.sc/lzpf" + k)
+		print ("\t\thttps://prnt.sc/lzpf" + m)
+		print ("\t\thttps://prnt.sc/lzpf" + n)
+		print ("\t\thttps://prnt.sc/lzpf" + o)
+		print ("\t\thttps://prnt.sc/lzpf" + q)
+		print ("\t\thttps://prnt.sc/lzpf" + s)
+		print ("\t\thttps://prnt.sc/lzpf" + u)
+		print ("\t\thttps://prnt.sc/lzpf" + x)
+		print ("\t\thttps://prnt.sc/lzpf" + w)
+		print ("\t\thttps://prnt.sc/lzpf" + z)
+		print ("\t\thttps://prnt.sc/lzpf" + ab)
+		print ("\t\thttps://prnt.sc/lzpf" + ba)
+		print ("\t\thttps://prnt.sc/lzpf" + ac)
+		print ("\t\thttps://prnt.sc/lzpf" + ca)
+		print ("\t\thttps://prnt.sc/lzpf" + ad)
+		print ("\t\thttps://prnt.sc/lzpf" + da)
+		print ("\t\thttps://prnt.sc/lzpf" + ae)
+		print ("\t\thttps://prnt.sc/lzpf" + ea)
+		print ("\t\thttps://prnt.sc/lzpf" + af)
+		print ("\t\thttps://prnt.sc/lzpf" + fa)
+		print ("\t\thttps://prnt.sc/lzpf" + ag)
+		print ("\t\thttps://prnt.sc/lzpf" + ga)
+		print ("\t\thttps://prnt.sc/lzpf" + ha)
+		print ("\t\thttps://prnt.sc/lzpf" + ah)
+		print ("\t\thttps://prnt.sc/lzpf" + ia)
+		print ("\t\thttps://prnt.sc/lzpf" + aj)
+		print ("\t\thttps://prnt.sc/lzpf" + ak)
+		print ("\t\thttps://prnt.sc/lzpf" + ka)
+		print ("\t\thttps://prnt.sc/lzpf" + am)
+		print ("\t\thttps://prnt.sc/lzpf" + ma)
+		print ("\n\n_____________________________________________\n")
+	if variant == 3:
+		cls()
+	if variant == 4:
+		cls()
+		print ("    / / / /     |    /             _            |      |")
+		print ("   / /          |   /             / \\           |      |")
+		print ("  / /           |  /             /   \\          |      |")
+		print (" / /            | /             /     \\         |      |")
+		print ("/ /             |/             /_______\\        |______|")
+		print ("\\ \\             |\\            /         \\       |      |")
+		print (" \\ \\            | \\          |           |      |      |")
+		print ("  \\ \\           |  \\         |           |      |      |")
+		print ("   \\ \\ \\ \\      |   \\        |           |      |      |")
+		print ("                     PrintScreen.com                    ")
+		os.system("pause")
+		exit()
+	while True:
+		scaner()
+scanMain()
