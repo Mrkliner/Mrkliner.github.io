a44b309010cd3f107e85f8b84a977c26245626b0
Mrkliner <78759365+Mrkliner@users.noreply.github.com>
2022-02-21 15:56:13 +0300

    Update Tags.js

--- Pages/Games/Tags.js
+++ Pages/Games/Tags.js
@@ -4,3 +4,9 @@ function customButton(tagname) {
 	        tag[i].innerHTML = '<br><button style="display: flex;align-items: center;">' + tag[i].attributes.text.value + '</button>';
         }
 }
+function customText(tagname) {
+	var tag = document.getElementsByTagName(tagname);
+	for (var i = 0; i < tag.length; i++) {
+		tag[i].innerHTML = '<br><h3 class="standart-text">' + tag[i].attributes.text.value + '</h3>'
+	}
+}
